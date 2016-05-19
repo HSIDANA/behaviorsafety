e6558c1ab205e4124c393c6b062acd5943d347c0
harshita <s.harshita.89@gmail.com>
2016-05-19 18:39:17 +0930

    initoal

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
-var express = require('express');
-var app = express();
-
-app.use(express.static(__dirname + '/public'));
-
-var server = app.listen(process.env.PORT || 80, function () {
-  var port = server.address().port;
-  console.log('Listening at port ' + port);
+var express = require('express'),
+app = express();
+app.use(express.static('www'));
+app.set('port', process.env.PORT || 5000);
+app.listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
 });
